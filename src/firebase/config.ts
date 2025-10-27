e2b6e42fd56dfa3a1a0e50192dfcc62759e88431
Kip russ <kipruss00@gmail.com>
2025-10-27 05:24:51 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6766352949-446a0",
+  "appId": "1:548336738009:web:58c02e1aa2dbdb837da8c8",
+  "apiKey": "AIzaSyC4qwyXxD8x6-oBq6Hj2aHJhwdYQw_empE",
+  "authDomain": "studio-6766352949-446a0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "548336738009"
+};
