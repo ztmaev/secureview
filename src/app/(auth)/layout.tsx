fe299c450f5894c441871f5c01eb77b1424f9a23
Kip russ <kipruss00@gmail.com>
2025-10-28 07:28:14 +0000

    i hope what youre fixing will help me share this progress not the app

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,17 +1,13 @@
 'use client';
 
-import { FirebaseClientProvider } from '@/firebase/client-provider';
-
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <FirebaseClientProvider>
-      <div className="flex min-h-screen flex-col items-center justify-center p-4">
-        {children}
-      </div>
-    </FirebaseClientProvider>
+    <div className="flex min-h-screen flex-col items-center justify-center p-4">
+      {children}
+    </div>
   );
 }
